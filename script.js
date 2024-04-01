2dd1cf0789472b10278589fbf64db483c35520e3
nemanjamalin <147262488+nemanjamalin@users.noreply.github.com>
2024-04-01 17:36:22 +0200

    generate random choice from computer

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert('woks');
+function getComputerChoice(){
+    const choices = ['rock','paper','scissors'];
+    const choiceIndex = Math.floor(Math.random()*3);
+
+    return choices[choiceIndex];
+} // basically we have an array with choices, then we generate random number from 0 - 2
+// then we return item from choices with random number generated as index of choices
+
+// console.log(getComputerChoice()); tested if computer choice works
+
+
