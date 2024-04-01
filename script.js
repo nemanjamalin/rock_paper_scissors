56d5e2691c5242ad821de3b6ea99192f479cdfa9
nemanjamalin <147262488+nemanjamalin@users.noreply.github.com>
2024-04-01 18:07:57 +0200

    implement score system

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     const choices = ['rock','paper','scissors'];
     const choiceIndex = Math.floor(Math.random()*3);
@@ -20,15 +23,30 @@ function playRound(playerSelection,computerSelection){
     
     if(playerSelection === computerSelection) return tieMessage;
 
-    if(playerSelection === 'Rock' && computerSelection === 'Scissors' || playerSelection === 'Paper' && computerSelection === 'Rock' || playerSelection === 'Scissors' && computerSelection === 'Paper') return winMessage;
+    if(playerSelection === 'Rock' && computerSelection === 'Scissors' || playerSelection === 'Paper' && computerSelection === 'Rock' || playerSelection === 'Scissors' && computerSelection === 'Paper') 
+        {
+            playerScore++; //increment players score
+            return winMessage; //return message
+        }
+    
 
-    if(computerSelection === 'Rock' && playerSelection === 'Scissors' || computerSelection === 'Paper' && playerSelection === 'Rock' || computerSelection === 'Scissors' && playerSelection === 'Paper') return lostMessage;
+    if(computerSelection === 'Rock' && playerSelection === 'Scissors' || computerSelection === 'Paper' && playerSelection === 'Rock' || computerSelection === 'Scissors' && playerSelection === 'Paper') 
+        {
+            computerScore++; //increment computers score
+            return lostMessage; //return message
+        }
+   
 
 
 
 } // not so good way to determine if you win or lose, but works for this purpose
 
 
-console.log(playRound('rOCK',getComputerChoice()));
+const playerSelection = prompt('Write your choice:');
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection,computerSelection));
+console.log('player score:',playerScore);
+console.log('computer score:',computerScore);
 
 
