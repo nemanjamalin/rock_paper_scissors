6ca59ad13fe778d33de9017e2188c51d9744e1d5
nemanjamalin <147262488+nemanjamalin@users.noreply.github.com>
2024-04-01 17:56:11 +0200

    make round function

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ function getComputerChoice(){
 // console.log(getComputerChoice()); tested if computer choice works
 
 
+function playRound(playerSelection,computerSelection){
+
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+    computerSelection = computerSelection[0].toUpperCase() + computerSelection.slice(1).toLowerCase();;
+
+    const winMessage = `You Win! ${playerSelection} beats ${computerSelection}`;
+    const lostMessage = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    const tieMessage = `It's a tie!`;
+    
+    if(playerSelection === computerSelection) return tieMessage;
+
+    if(playerSelection === 'Rock' && computerSelection === 'Scissors' || playerSelection === 'Paper' && computerSelection === 'Rock' || playerSelection === 'Scissors' && computerSelection === 'Paper') return winMessage;
+
+    if(computerSelection === 'Rock' && playerSelection === 'Scissors' || computerSelection === 'Paper' && playerSelection === 'Rock' || computerSelection === 'Scissors' && playerSelection === 'Paper') return lostMessage;
+
+
+
+} // not so good way to determine if you win or lose, but works for this purpose
+
+
+console.log(playRound('rOCK',getComputerChoice()));
+
+
