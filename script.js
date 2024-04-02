64eae254e9dec7ddbecfef4b7e1785347d7c1fa9
nemanjamalin <147262488+nemanjamalin@users.noreply.github.com>
2024-04-02 19:33:33 +0200

    structure code better, validate user input

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 let playerScore = 0;
 let computerScore = 0;
 
+
+const CHOICES = ['rock','paper','scissors'];
+
+
+
+function getCapitalizedWord(word){
+    word = word[0].toUpperCase() + word.slice(1).toLowerCase();
+    return word;
+}
+
 function getComputerChoice(){
-    const choices = ['rock','paper','scissors'];
     const choiceIndex = Math.floor(Math.random()*3);
 
-    return choices[choiceIndex];
-} // basically we have an array with choices, then we generate random number from 0 - 2
-// then we return item from choices with random number generated as index of choices
+    return CHOICES[choiceIndex];
+}
 
-// console.log(getComputerChoice()); tested if computer choice works
+function getPlayerChoice(){
+    const playerSelection = prompt('Please choose rock/paper/scissors:').toLowerCase();
+    console.log(playerSelection);
+    if(CHOICES.includes(playerSelection)) return playerSelection;
+
+    alert('Not valid, try again!')
+    return getPlayerChoice();
+}
+
+
 
 
 function playRound(playerSelection,computerSelection){
 
-    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
-    computerSelection = computerSelection[0].toUpperCase() + computerSelection.slice(1).toLowerCase();;
+    playerSelection = getCapitalizedWord(playerSelection);
+    computerSelection = getCapitalizedWord(computerSelection);
 
     const winMessage = `You Win! ${playerSelection} beats ${computerSelection}`;
     const lostMessage = `You Lose! ${computerSelection} beats ${playerSelection}`;
@@ -25,15 +42,15 @@ function playRound(playerSelection,computerSelection){
 
     if(playerSelection === 'Rock' && computerSelection === 'Scissors' || playerSelection === 'Paper' && computerSelection === 'Rock' || playerSelection === 'Scissors' && computerSelection === 'Paper') 
         {
-            playerScore++; //increment players score
-            return winMessage; //return message
+            playerScore++; 
+            return winMessage; 
         }
     
 
     if(computerSelection === 'Rock' && playerSelection === 'Scissors' || computerSelection === 'Paper' && playerSelection === 'Rock' || computerSelection === 'Scissors' && playerSelection === 'Paper') 
         {
-            computerScore++; //increment computers score
-            return lostMessage; //return message
+            computerScore++; 
+            return lostMessage; 
         }
    
 
@@ -42,25 +59,28 @@ function playRound(playerSelection,computerSelection){
 } // not so good way to determine if you win or lose, but works for this purpose
 
 
-function playGame(){
-    const playerSelection = prompt('Write your choice:'); // receive user input from prompt as choice
-    const computerSelection = getComputerChoice(); // get computers choice
+function playGame(roundNumber){
+    const playerSelection = getPlayerChoice(); 
+    const computerSelection = getComputerChoice(); 
+    const roundWinnerMessage = playRound(playerSelection,computerSelection);
 
-    console.log(playRound(playerSelection,computerSelection)); // console log who wins current round
-    console.log('player score:',playerScore); // write score of player
-    console.log('computer score:',computerScore); // write score of player
-    console.log('----------------------------------------'); // for easier reading of console
-}// function playGame which plays one round each team it's called and updates the console with current scores
+    alert(`
+        round ${roundNumber}/5
+        ${roundWinnerMessage}
+        player score: ${playerScore}
+        computer score: ${computerScore}
+    `)
+}
 
 for(let i=0;i<5;i++){
-    playGame(); // for loop to call playGame function 5 times
+    playGame(i+1); 
 }
 
 playerScore > computerScore ?
-    console.log('Player wins out of 5 rounds')
+    alert('Player wins out of 5 rounds')
 : playerScore < computerScore ?
-    console.log('Computer wins out of 5 rounds')
-:   console.log(`It's a tie out of 5 rounds`); //tenary operator, to determine a winner
+    alert('Computer wins out of 5 rounds')
+:   alert(`It's a tie out of 5 rounds`); //tenary operator, to determine a winner
 
 
 
